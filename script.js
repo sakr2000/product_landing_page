58e2f94a0b1e549331f5bf5931fbd13ca6935955
mohamed_sakr <sakr83117@gmail.com>
2023-09-25 23:19:05 +0300

    added media queries

---
+++ script.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".header nav");
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
