397a4308ada33f1f4673a4310c0649e7fca1fd4d
mohamed_sakr <sakr83117@gmail.com>
2023-09-28 15:07:27 +0300

    toggle nav in small screens

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let menu = document.querySelector("#menu-btn");
 let navbar = document.querySelector(".header nav");
+let navbtns = document.querySelectorAll(".header nav a");
 menu.onclick = () => {
   menu.classList.toggle("fa-times");
   navbar.classList.toggle("active");
 };
+navbtns.forEach((Element) => {
+  Element.addEventListener("click", () => {
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+  });
+});
